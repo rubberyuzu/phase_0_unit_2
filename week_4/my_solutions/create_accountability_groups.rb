ca83ea4700343587d615fe2f726cd186ad253738
rubberyuzu <yuzu0125@gmail.com>
2014-04-01 09:36:08 -0700

    deleted some unnecessary folders

---
+++ week_4/my_solutions/create_accountability_groups.rb
@@ -0,0 +1,54 @@
+# U2.W4: challenge_1
+
+
+# I worked on this challenge by myself
+
+# 2. Pseudocode
+
+# Input: an array
+# Output: an array, containing arrays with 4 (or less) elements each
+# Steps:
+# define create_group
+# shuffle the array
+# each_slice(4)
+# do this for 3 times
+
+
+# 3. Initial Solution
+
+def create_group(array)
+  
+  new_array=array.shuffle
+  groups = new_array.each_slice(2).to_a
+
+end
+
+
+
+# 4. Refactored Solution
+
+
+
+
+
+
+
+
+# 1. DRIVER TESTS GO BELOW THIS LINE
+# 
+# if the method takes one argument ?
+# if the method returns 3 arrays
+# if each array contains 8 arrays
+# if 
+puts 
+
+
+
+
+
+# 5. Reflection 
+
+
+
+
+
