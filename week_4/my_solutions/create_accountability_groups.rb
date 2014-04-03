cc4a993390a2932a1f590bce5f5e97416ca8f5c8
rubberyuzu <yuzu0125@gmail.com>
2014-04-02 18:16:05 -0700

    completed challenge1

--- week_4/my_solutions/create_accountability_groups.rb
+++ week_4/my_solutions/create_accountability_groups.rb
@@ -11,22 +11,24 @@
 # define create_group
 # shuffle the array
 # each_slice(4)
-# do this for 3 times
+
 
 
 # 3. Initial Solution
 
 def create_group(array)
   
-  new_array=array.shuffle
-  groups = new_array.each_slice(2).to_a
-
+  new_array = array.shuffle
+  return new_array.each_slice(4).to_a
 end
 
 
 
 # 4. Refactored Solution
 
+def create_group(array)
+  array.shuffle.each_slice(4).to_a
+end
 
 
 
@@ -35,19 +37,36 @@ end
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
-# 
-# if the method takes one argument ?
-# if the method returns 3 arrays
-# if each array contains 8 arrays
-# if 
-puts 
+array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+create_group(array).length == array.length/4+1
 
-
-
+if array.length >= 5
+ for i in 0..create_group(array).length-2 do 
+ puts create_group(array)[i].length == 4
+ i+=1
+end
+else
+ puts create_group(array)[0].length == array.length
+end
 
 
 # 5. Reflection 
+# Firstly I tried to write a code which takes a number of how many times you would like to get
+# the accountability groups like below, which did not work.
+# I would like to figure out why.
+# 
+def create_group(array)
+  
+  new_array = array.shuffle
+  return new_array.each_slice(4).to_a
+end
+def accountability)(array,number)
+number.times{
+	create_group(array)
+}
+end
 
+# Also I did not use non-destructive methods at all, and don't know where to use it....
 
 
 
